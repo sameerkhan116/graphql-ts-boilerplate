b6f7de8f36d6a50715e7b01f05f9667f5010350d
Sameer Khan <sameer_khan@student.uml.edu>
2018-06-14 12:47:02 -0400

    More optimization

--- src/modules/register/resolvers.ts
+++ src/modules/register/resolvers.ts
@@ -12,13 +12,28 @@ export const resolvers: ResolverMap = {
       _,
       { email, password }: GQL.IRegisterOnMutationArguments,
     ) => {
+      const userAlreadyExists = await User.findOne({
+        where: { email },
+        select: ["id"],
+      });
+
+      if (userAlreadyExists) {
+        return [
+          {
+            path: "email",
+            message: "User already exists.",
+          },
+        ];
+      }
+
       const hashedPassword = await bcrypt.hash(password, 10);
       const user = User.create({
         email,
         password: hashedPassword,
       });
       await user.save();
-      return true;
+
+      return null;
     },
   },
 };
